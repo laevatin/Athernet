787dbd9fb0baac8abb1f827953e25e42c107928a
lizch <lizch@shanghaitech.edu.cn>
2021-10-11 15:26:15 +0800

    add ringbuffer

---
+++ Source/Ringbuffer.hpp
@@ -0,0 +1,74 @@
+#ifndef __RINGBUFFER_HPP__
+#define __RINGBUFFER_HPP__
+
+#include <type_traits>
+#include <cstring>
+#include <iostream>
+
+#define CAPACITY 65536
+
+/**
+ * This ring buffer is fixed size of 65536 elements
+ * since 65536 is the size of uint16_t
+ */
+
+template <typename Type>
+class RingBuffer
+{
+    static_assert(std::is_integral<Type>::value, "Integral type required.");
+
+public:
+    RingBuffer() {};
+    ~RingBuffer() {};
+
+    void write(const Type* data, std::size_t len)
+    {
+        if (fillCount + len >= CAPACITY)
+            std::cerr << "Ring Buffer Overflow\n";
+
+        if ((uint16_t)(head + len) < head)
+        {
+            memcpy(buffer + head, data, sizeof(Type) * (UINT16_MAX - head));
+            memcpy(buffer, data + (UINT16_MAX - head), sizeof(Type) * (uint16_t)(head + len));
+        }
+        else
+            memcpy(buffer + head, data, sizeof(Type) * len);
+
+        head += len;
+        fillCount += len;
+    }
+
+    void read(Type* data, std::size_t len)
+    {
+        if (fillCount < len)
+            std::cerr << "Ring Buffer Underflow\n";
+
+        if ((uint16_t)(tail + len) < tail)
+        {
+            memcpy(data, buffer + tail, sizeof(Type) * (UINT16_MAX - tail));
+            memcpy(data + (UINT16_MAX - tail), buffer, sizeof(Type) * (uint16_t)(tail + len));
+        }
+        else
+        {
+            memcpy(data, buffer + tail, sizeof(Type) * len);
+        }
+
+
+        tail += len;
+        fillCount -= len;
+    }
+
+    std::size_t size()
+    {
+        return fillCount;
+    }
+
+private:
+    Type buffer[CAPACITY];
+    uint16_t head = 0;
+    uint16_t tail = 0;
+    std::size_t fillCount = 0;
+};
+
+
+#endif  
