fc113a7a08416fdea56dd27f757954e83ca4073f
lizch <lizch@shanghaitech.edu.cn>
2021-10-24 23:41:23 +0800

    add config

--- Include/Config.h
+++ Include/Config.h
@@ -30,16 +30,25 @@ public:
     constexpr static int DATA_PER_FRAME = 8 * 62;
     constexpr static int BIT_PER_FRAME  = 8 * 72;
 
+    constexpr static int BIT_PER_ACK  = 40;
+
     constexpr static int BAND_WIDTH   = 2;
 
     constexpr static int SAMPLE_PER_FRAME = BIT_PER_FRAME * BIT_LENGTH / BAND_WIDTH;
     constexpr static int FRAME_LENGTH     = SAMPLE_PER_FRAME + HEADER_LENGTH + 10;
+    constexpr static int ACK_LENGTH       = HEADER_LENGTH + BIT_LENGTH * BIT_PER_ACK;
+
+    constexpr static uint8_t ACK = 0xAC;
+    constexpr static uint8_t DATA = 0xDA;
+
+    constexpr static uint8_t SENDER = 0xED;
+    constexpr static uint8_t RECEIVER = 0xCE;
 
     constexpr static int RECV_TIMEOUT = 1;
 
     constexpr static int PENDING_QUEUE_SIZE = 20;
 
-    constexpr static enum AudioDevice::state STATE = AudioDevice::state::BOTH;
+    constexpr static enum AudioIO::state STATE = AudioIO::state::BOTH;
 
     static AudioType header;
     static std::vector<AudioType> modulateSound;
