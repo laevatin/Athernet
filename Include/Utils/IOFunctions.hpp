1902925469d558ac1ed8514e2a51ec93fe0ce23f
lizch <lizch@shanghaitech.edu.cn>
2021-10-25 23:12:08 +0800

    better IOFunctions

--- Include/Utils/IOFunctions.hpp
+++ Include/Utils/IOFunctions.hpp
@@ -6,11 +6,8 @@
 #include "Physical/Audio.h"
 
 /* Here `inline` hints the linker to choose arbitary one definition. */
-inline DataType bitToByte(const DataType &bitArray)
+inline void __bitToByte(const DataType &bitArray, DataType &byteArray)
 {
-    DataType byteArray;
-    byteArray.resize(bitArray.size() / 8);
-
     for (int i = 0; i < bitArray.size(); i += 8) 
     {
         uint8_t val = 0;
@@ -18,20 +15,29 @@ inline DataType bitToByte(const DataType &bitArray)
             val |= bitArray[i + j] << ((i + j) % 8);
         byteArray.set(i / 8, val);  
     }
+}
 
+inline DataType bitToByte(const DataType &bitArray)
+{
+    DataType byteArray;
+    byteArray.resize(bitArray.size() / 8);
+    __bitToByte(bitArray, byteArray);
     return std::move(byteArray);
 }
 
-inline DataType byteToBit(const DataType &byteArray)
+inline DataType __byteToBit(DataType &bitArray,const DataType &byteArray)
 {
-    DataType bitArray;
-
     bitArray.resize(byteArray.size() * 8);
     for (int i = 0; i < bitArray.size(); i++) 
     {
         bitArray.set(i, ((1 << (i % 8)) & (byteArray[i / 8])) >> (i % 8));
     }
+}
 
+inline DataType byteToBit(const DataType &byteArray)
+{
+    DataType bitArray;
+    __byteToBit(bitArray, byteArray);
     return std::move(bitArray);
 }
 
