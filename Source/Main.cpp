6fdae2c6cf1ae631987bb60ab5d8772b5e280d88
zengsx <zengsx@shanghaitech.edu.cn>
2021-11-20 01:11:50 +0800

    command?

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,10 +4,14 @@
 #include <bitset>
 #include <fstream>
 #include <chrono>
+#include <iostream>
+#include <string>
 
 extern std::ofstream debug_file;
 #define PI acos(-1)
-
+std::string m_exit = "exit";
+std::string macperf= "macperf ";
+std::string macping= "macping";
 void getInputFromFile(Array<uint8_t> &input, const std::string &path)
 {
     std::ifstream inputFile;
@@ -34,10 +38,37 @@ int main(int argc, char* argv[])
 {
     MessageManager::getInstance();
     AudioIO audioIO;
-    
+	std::string Ifile;
+	std::string Ofile;
+	std::string command;
     Array<uint8_t> input;
     Array<uint8_t> output;
-    getInputFromFile(input, "C:\\Users\\16322\\Desktop\\lessons\\2021_1\\CS120_Computer_Network\\Athernet-cpp\\Input\\input50000.in");
+
+	//std::cout << "Please enter the type of this device.(Sender:1 Receiver:2 Both:3)"<<newLine;
+	std::cout << "Please enter command\n";
+	std::getline(std::cin, command);
+	std::cout << command.find("macping");
+	if (command.find("macping ") == 0) {
+		command.erase(0, 8);
+		std::cout << command << std::endl;
+	}
+	if (command.find("macperf ") == 0) {
+		command.erase(0, 8);
+		std::cout << command << std::endl;
+	}
+
+	std::cout << "Please enter the address of input file.\n";
+	std::getline(std::cin, Ifile);
+	std::cout <<"Input file: "<<Ifile << newLine;
+
+	std::cout << "Please enter the address of output file.\n";
+	std::getline(std::cin, Ofile);
+	std::cout << "Output file: " << Ofile << newLine;
+
+
+
+	
+    getInputFromFile(input, Ifile);
     // for (int i = 0; i < input.size(); i++) {
     //     std::cout << (int)input[i];
     //     if (i % (57 * 8) == 0) {
@@ -52,8 +83,8 @@ int main(int argc, char* argv[])
     std::cout << "Initialization finished.\n";
     while (getchar()) 
     {
-        outputfile.open("C:\\Users\\16322\\Desktop\\lessons\\2021_1\\CS120_Computer_Network\\Athernet-cpp\\Input\\output50000.out");
-        debug_file.open("C:\\Users\\16322\\Desktop\\lessons\\2021_1\\CS120_Computer_Network\\Athernet-cpp\\Input\\debug.out");
+        outputfile.open(Ofile);
+        debug_file.open("C:\\Users\\zengs\\Desktop\\homework\\CS120\\Project\\Athernet\\Input\\debug.out");
 
         auto now1 = std::chrono::system_clock::now();
         audioIO.startTransmit();
