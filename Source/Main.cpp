ffa426d8d16617d5f18fbd0a6f264b2e4edc2355
zengsx <zengsx@shanghaitech.edu.cn>
2021-11-20 19:55:27 +0800

    some conflicts after merge

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -43,6 +43,14 @@ int main(int argc, char* argv[])
     Array<uint8_t> output;
 
 	//std::cout << "Please enter the type of this device.(Sender:1 Receiver:2 Both:3)"<<newLine;
+	std::cout << "Please enter the address of input file.\n";
+	std::getline(std::cin, Ifile);
+	std::cout << "Input file:\n" << Ifile << newLine;
+
+	std::cout << "Please enter the address of output file.\n";
+	std::getline(std::cin, Ofile);
+	std::cout << "Output file:\n" << Ofile << newLine;
+
 	std::cout << "Please enter command\n";
 	std::getline(std::cin, command);
 	if (command.find("macping ") == 0) {
@@ -53,21 +61,6 @@ int main(int argc, char* argv[])
 		command.erase(0, 8);
 		std::cout << command << std::endl;
 	}
-	else {
-		std::cout << "Please enter the address of input file.\n";
-		std::getline(std::cin, Ifile);
-		std::cout << "Input file: " << Ifile << newLine;
-
-		std::cout << "Please enter the address of output file.\n";
-		std::getline(std::cin, Ofile);
-		std::cout << "Output file: " << Ofile << newLine;
-	}
-	
-
-
-
-
-
 	
     getInputFromFile(input, Ifile);
     // for (int i = 0; i < input.size(); i++) {
@@ -82,8 +75,7 @@ int main(int argc, char* argv[])
     audioIO.write(input);
 
     std::cout << "Initialization finished.\n";
-	
-    while (getchar()!= 101) 
+    while (getchar()) 
     {
         outputfile.open(Ofile);
         debug_file.open(debugFile);
@@ -100,7 +92,6 @@ int main(int argc, char* argv[])
         outputfile.close();
         debug_file.close();
         audioIO.write(input);
-		std::cout << "Press e to exit\n";
     }
 
     return 0;
